d6c9fcb551e548b29cc6311ee0f426c200ae1703
barretowski <phenriquevb@hotmail.com>
2023-05-22 16:21:24 -0300

    Feat: Declaração da árvore patricia

---
+++ src/No.java
@@ -0,0 +1,30 @@
+public class No {
+    private String palavra;
+    private boolean flag;
+    private No vLig[];
+
+    public String getPalavra() {
+        return palavra;
+    }
+
+    public void setPalavra(String palavra) {
+        this.palavra = palavra;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public No[] getvLig() {
+        return vLig;
+    }
+
+    public void setvLig(No[] vLig) {
+        this.vLig = vLig;
+    }
+
+}
